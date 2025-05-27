64f372678b0329d25d0b6ae872a8f58f0907742d
andystar22 <31457164+andystar22@users.noreply.github.com>
2025-05-27 14:21:24 -0500

    Update Socializing.java

--- src/main/java/com/gmail/nossr50/skills/socializing/Socializing.java
+++ src/main/java/com/gmail/nossr50/skills/socializing/Socializing.java
@@ -1,1 +1,7 @@
-
+import com.gmail.nossr50.datatypes.skills.SubSkillType;
+import com.gmail.nossr50.mcMMO;
+import com.gmail.nossr50.util.ItemUtils;
+import com.gmail.nossr50.util.skills.RankUtils;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
